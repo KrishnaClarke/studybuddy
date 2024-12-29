22f8b42f23a890f28ae73f25dfc0e7a987d76f70
Krishna Clarke <126019343+KrishnaClarke@users.noreply.github.com>
2024-12-29 19:51:00 -0400

    small changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <header>
-      <h1>Study Buddy</h1>
-    </header>
+    <div>
+      <body>
+        <header>
+          <h1 className="header">Study Buddy</h1>
+        </header>
+
+        <div></div>
+
+        <footer className="footer">
+          <p>Study Buddy</p>
+          <img></img>
+        </footer>
+      </body>
+    </div>
   );
 }
