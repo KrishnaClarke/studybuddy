14c779692d7f02b0402df6c6cd46c1eb5f7c2780
Krishna Clarke <126019343+KrishnaClarke@users.noreply.github.com>
2024-12-29 20:08:53 -0400

    svg added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,9 @@
 import Image from "next/image";
+import Home from "../components/Home";
+import Messages from "../components/Messages";
+import Calander from "../components/Calander";
+import AddReminder from "../components/AddReminder";
+import EditReminder from "../components/EditReminder";
 
 export default function Home() {
   return (
